f81e09cf0800170c5d5b1b2c75da951ca3019f91
web-dev <nathankim137311@gmail.com>
2021-10-12 19:45:11 -0700

    added text content to divs, and added black border around card

--- src/main.js
+++ src/main.js
@@ -21,19 +21,24 @@ function main() {
     const hoursH2 = document.createElement('h2');
     hoursH2.classList.add('card-headings');
     hoursH2.textContent = 'Hours';
-    hours.appendChild(hoursH2); 
+    const hoursP = document.createElement('p');
+    hoursP.textContent = 'M-F: 8pm to 3am';
+    const hoursP2 = document.createElement('p');
+    hoursP2.textContent = 'Sat: 6pm to 12am';
+    const hoursP3 = document.createElement('p');
+    hoursP3.textContent = 'Sunday: closed'; 
+    hours.append(hoursH2, hoursP, hoursP2, hoursP3); 
     // locations sections 
     const location = document.createElement('div');
     location.classList.add('card-div');
     const locationH2 = document.createElement('h2');
     locationH2.classList.add('card-headings');
     locationH2.textContent = 'Find Us';
-    location.appendChild(locationH2);
+    const locationP = document.createElement('p');
+    locationP.textContent = '1223 fake st SE, fake city, 54321, state, USA';
+    location.append(locationH2, locationP);
 
 
-
-    const p = document.createElement('p'); 
-    p.textContent = 'Korean Street makes the best korean street food in k-town, with a variety of options like tteok-bokki, gimbap, bibimbap, corn dogs, and delicious seafood pajeon, all delivered with a smile and fast service. '
     content.appendChild(main);
     main.appendChild(infoCard); 
     infoCard.append(about, hours, location);
